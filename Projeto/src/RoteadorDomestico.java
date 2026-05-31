d583c20f866342e1fe1051295725d5ab9b4cddd1
mecrym <maria.ecr.moreira1@gmail.com>
2026-05-31 04:00:21 -0300

    feat(RoteadorDomestico): implements parental control activation method and getters and setters

--- Projeto/src/RoteadorDomestico.java
+++ Projeto/src/RoteadorDomestico.java
@@ -9,7 +9,14 @@ public class RoteadorDomestico extends Roteador implements CompartilhamentoUSB{
     }
 
 
-    public void ativarControleParental(){}
+    public void ativarControleParental(){
+        //TO-DO: show this to Lidia, because it still doesn't make sense to me to have a method that only prints something...
+        if (controleParentalAtivo) {
+            System.out.println("O controle parental foi ativado com sucesso. Agora você terá acesso ao que a sua criança acessar e conseguirá bloquear conteudos indesejados.");
+        } else {
+            System.out.println("Você optou por não ativar o controle parental. Caso mude de ideia, é só acessar o nosso menu de configurações.");
+        }
+    }
 
     //superclass methods
     //TO-DO: The code is NOT as it should be, based on the UML, u must update it to match the code (yep, good luck...)
@@ -32,5 +39,14 @@ public class RoteadorDomestico extends Roteador implements CompartilhamentoUSB{
     @Override
     public boolean ejetarUnidade() {
         return false;
+    }
+
+    //getters and setters
+    public boolean isControleParentalAtivo() {
+        return controleParentalAtivo;
+    }
+
+    public void setControleParentalAtivo(boolean controleParentalAtivo) {
+        this.controleParentalAtivo = controleParentalAtivo;
     }
 }

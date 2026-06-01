abd0a6567b70725b19c58c2cdef33610ce45d667
mecrym <maria.ecr.moreira1@gmail.com>
2026-06-01 20:58:16 -0300

    feat(RoteadorDomestico): update constructor and add IP file reading method

--- Projeto/src/RoteadorDomestico.java
+++ Projeto/src/RoteadorDomestico.java
@@ -1,13 +1,19 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+
 public class RoteadorDomestico extends Roteador implements CompartilhamentoUSB{
     private String senhaWifi;
     private boolean controleParentalAtivo;
 
-    public RoteadorDomestico(String marca, String modelo, double preco, String gateway, String senhaWifi, boolean controleParentalAtivo){
+    public RoteadorDomestico(String marca, String modelo, double preco, String gateway, String senhaWifi, boolean controleParentalAtivo, String ipRoteador) {
         super(marca, modelo, preco, gateway);
         this.senhaWifi = senhaWifi;
         this.controleParentalAtivo = controleParentalAtivo;
+        this.ipRoteador = null;
     }
-
 
     public void ativarControleParental(){
         //TO-DO: show this to Lidia, because it still doesn't make sense to me to have a method that only prints something...
@@ -18,10 +24,21 @@ public class RoteadorDomestico extends Roteador implements CompartilhamentoUSB{
         }
     }
 
+    private List<String>lerArquivosIPs(){
+        try{
+            return Files.readAllLines(Paths.get("Projeto/arquivo.txt"));
+        }catch(IOException e){
+            System.out.println("Erro ao ler lista de ips: " + e.getMessage());
+            return List.of();
+        }
+    }
+
     //superclass methods
     //TO-DO: The code is NOT as it should be, based on the UML, u must update it to match the code (yep, good luck...)
     @Override
-    public void atribuirIP(String ip){}
+    public void atribuirIP(String ip){
+
+    }
 
     @Override
     public void bloquearSite(String url){}
